d9205fc6cc2e914565e25180085005d78bf387ca
JlordS32 <jlordsty128@gmail.com>
2023-11-25 10:08:16 +1100

    arrays and object literals

---
+++ netninja/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// --------
+// arrays
+// --------
+let names = ['sdfsdf', 'sdfsd', 'sdf'];
+let ages = [1, 1, 1, 1, 1];
+let things = [1, true, 'hello'];
+console.log(things);
+console.log(names);
+const person = {
+    name: 'John',
+    age: 123,
+    something: 243,
+    theUnitedStatesofWhatever: {
+        name: 'Something',
+        age: '1000',
+    },
+};
