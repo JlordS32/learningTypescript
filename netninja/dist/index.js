05651f6548b0c75ba087c0cc4b3463fcd3e06597
JlordS32 <jlordsty128@gmail.com>
2023-11-25 15:35:00 +1100

    done

--- netninja/dist/index.js
+++ netninja/dist/index.js
@@ -1,1 +1,27 @@
 "use strict";
+// extending interfaces
+const user = {
+    id: 1,
+    format() {
+        return `This user has an id of ${this.id}`;
+    },
+};
+const bill = {
+    id: 2,
+    amount: 50,
+    server: 'mario',
+    format() {
+        return `This user has an id of ${this.id}`;
+    },
+};
+function printFormatted(val) {
+    console.log(val.format());
+}
+function printBill(val) {
+    console.log('server: ', val.server);
+    console.log(val.format());
+}
+printFormatted(user);
+printFormatted(bill);
+printBill(bill);
+printBill(user); // error
