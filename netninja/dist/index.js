84f9489222ce4592e787dc8c158c8d45b8d419f1
JlordS32 <jlordsty128@gmail.com>
2023-11-27 09:06:56 +1100

    commit

--- netninja/dist/index.js
+++ netninja/dist/index.js
@@ -1,44 +1,12 @@
 "use strict";
-// Generic Interface
-// -------------------
-const collectionOne = {
-    data: [1, 2, 3],
-    name: 'collectionOne',
-};
-const collectionTwo = {
-    data: ['apple', 'banana', 'orange'],
-    name: 'collectionOne',
-};
-const collectionThree = {
-    data: [
-        {
-            name: 'John',
-            age: 30,
-        },
-        {
-            name: 'Conor',
-            age: 19,
-        },
-        {
-            name: 'Dion',
-            age: 19,
-        },
-        {
-            name: 'Jaylou',
-            age: 21,
-        },
-    ],
-    name: 'collectionOne',
-};
-const collectionFour = {
-    data: ['death', 'driving', 'shit'],
-    name: 'collectionOne',
-};
-function randomCollectionItem(collection) {
-    return collection.data[Math.floor(Math.random() * collection.data.length)];
+// -----------------
+// Generic Classes
+// -----------------
+class DataCollection {
+    constructor(data) {
+        this.data = data;
+    }
+    loadOne() { }
+    loadAll() { }
+    add() { }
 }
-const randomDataOne = randomCollectionItem(collectionOne);
-const randomDataTwo = randomCollectionItem(collectionTwo);
-const randomDataThree = randomCollectionItem(collectionThree);
-const randomDataFour = randomCollectionItem(collectionFour);
-console.log({ randomDataOne, randomDataTwo, randomDataThree, randomDataFour });
--- netninja/src/index.ts
+++ netninja/src/index.ts
@@ -1,62 +1,12 @@
-// Generic Interface
-// -------------------
+// -----------------
+// Generic Classes
+// -----------------
 
-interface Collection<T> {
-	data: T[];
-	name: string;
+class DataCollection<T> {
+	constructor(private data: T[]) {}
+
+	loadOne() {}
+	loadAll() {}
+	add() {}
 }
 
-const collectionOne: Collection<number> = {
-	data: [1, 2, 3],
-	name: 'collectionOne',
-};
-
-const collectionTwo: Collection<string> = {
-	data: ['apple', 'banana', 'orange'],
-	name: 'collectionOne',
-};
-
-interface User {
-	name: string;
-	age: number;
-}
-
-const collectionThree: Collection<User> = {
-	data: [
-		{
-			name: 'John',
-			age: 30,
-		},
-		{
-			name: 'Conor',
-			age: 19,
-		},
-		{
-			name: 'Dion',
-			age: 19,
-		},
-		{
-			name: 'Jaylou',
-			age: 21,
-		},
-	],
-	name: 'collectionOne',
-};
-
-type dWords = 'driving' | 'death' | 'shit';
-
-const collectionFour: Collection<dWords> = {
-	data: ['death', 'driving', 'shit'],
-	name: 'collectionOne',
-};
-
-function randomCollectionItem<T>(collection: Collection<T>): T {
-	return collection.data[Math.floor(Math.random() * collection.data.length)];
-}
-
-const randomDataOne = randomCollectionItem(collectionOne);
-const randomDataTwo = randomCollectionItem(collectionTwo);
-const randomDataThree = randomCollectionItem(collectionThree);
-const randomDataFour = randomCollectionItem(collectionFour);
-
-console.log({randomDataOne, randomDataTwo, randomDataThree, randomDataFour});
