5f23d0595062427145455dd2e39a7b9bca3d9e75
JlordS32 <jlordsty128@gmail.com>
2023-11-27 09:13:58 +1100

    done

--- netninja/dist/index.js
+++ netninja/dist/index.js
@@ -6,7 +6,36 @@ class DataCollection {
     constructor(data) {
         this.data = data;
     }
-    loadOne() { }
-    loadAll() { }
-    add() { }
+    loadOne() {
+        const i = Math.floor(Math.random() * this.data.length);
+        const output = this.data[i];
+        console.log(output);
+        return output;
+    }
+    loadAll() {
+        console.log(this.data);
+        return this.data;
+    }
+    add(value) {
+        this.data.push(value);
+        console.log(this.data);
+        return this.data;
+    }
+    print() {
+        console.log(this.data);
+    }
 }
+const user = [
+    {
+        name: 'John',
+        age: 30,
+    },
+];
+const collection = new DataCollection(user);
+collection.loadOne();
+collection.loadAll();
+const newUser = {
+    name: 'John',
+    age: 21,
+};
+collection.add(newUser);
--- netninja/src/index.ts
+++ netninja/src/index.ts
@@ -5,8 +5,47 @@
 class DataCollection<T> {
 	constructor(private data: T[]) {}
 
-	loadOne() {}
-	loadAll() {}
-	add() {}
+	loadOne(): T {
+		const i = Math.floor(Math.random() * this.data.length);
+		const output = this.data[i];
+
+		console.log(output);
+		return output;
+	}
+	loadAll(): T[] {
+		console.log(this.data);
+		return this.data;
+	}
+	add(value: T): T[] {
+		this.data.push(value);
+		console.log(this.data);
+		return this.data;
+	}
+
+	print(): void {
+		console.log(this.data);
+	}
 }
 
+interface User {
+	name: string;
+	age: number;
+}
+
+const user = [
+	{
+		name: 'John',
+		age: 30,
+	},
+];
+
+const collection = new DataCollection<User>(user);
+
+collection.loadOne();
+collection.loadAll();
+
+const newUser = {
+	name: 'John',
+	age: 21,
+};
+collection.add(newUser);
