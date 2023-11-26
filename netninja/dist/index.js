a8a4f89ee2b2f70efeb039442434a9f58462d9f5
JlordS32 <jlordsty128@gmail.com>
2023-11-27 09:26:25 +1100

    some changes

--- netninja/dist/index.js
+++ netninja/dist/index.js
@@ -21,11 +21,23 @@ class DataCollection {
         console.log(this.data);
         return this.data;
     }
+    delete(id) {
+        const filteredData = (this.data = this.data.filter((x) => x.id !== id));
+        this.data = filteredData;
+    }
 }
 const users = new DataCollection([
     {
         name: 'jlord',
         age: 10000,
-        id: Math.floor(Math.random() * 1000),
+        id: 1,
+    },
+    {
+        name: 'zlord',
+        age: 10000,
+        id: 2,
     },
 ]);
+users.loadAll();
+users.delete(2);
+users.loadAll();
--- netninja/src/index.ts
+++ netninja/src/index.ts
@@ -25,6 +25,12 @@ class DataCollection<T extends HasID> {
 		console.log(this.data);
 		return this.data;
 	}
+
+	delete(id: number): void {
+		const filteredData = (this.data = this.data.filter((x) => x.id !== id));
+
+		this.data = filteredData;
+	}
 }
 
 interface User {
@@ -37,6 +43,15 @@ const users = new DataCollection<User>([
 	{
 		name: 'jlord',
 		age: 10000,
-		id: Math.floor(Math.random() * 1000),
+		id: 1,
+	},
+	{
+		name: 'zlord',
+		age: 10000,
+		id: 2,
 	},
 ]);
+
+users.loadAll();
+users.delete(2);
+users.loadAll();
