35ee0ffdaa02a5cfd99adf02359b16af27f3dcaa
JlordS32 <jlordsty128@gmail.com>
2023-11-27 10:33:28 +1100

    done

---
+++ netninja/src/ts/services/DataResource.ts
@@ -0,0 +1,20 @@
+export class DataResource<T> {
+   constructor(private endpoint: string) {
+   }
+
+   async loadAll() {
+
+   }
+
+   async loadOne(id: number) {
+
+   }
+
+   async add(id: number) {
+
+   }
+
+   async delete(data: T) {
+
+   }
+}
