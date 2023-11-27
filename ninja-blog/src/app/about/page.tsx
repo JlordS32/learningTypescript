9fe10761f176b4a0ef00c52254258267566893ca
JlordS32 <jlordsty128@gmail.com>
2023-11-27 14:00:41 +1100

    finished

---
+++ ninja-blog/src/app/about/page.tsx
@@ -0,0 +1,27 @@
+export default function About() {
+	return (
+		<main>
+			<h2>About</h2>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique
+				sequi architecto minus fugit incidunt dolores accusamus non provident
+				culpa, consectetur rerum saepe asperiores facilis, facere, autem
+				perferendis. Iusto, ullam omnis.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus qui error
+				recusandae magnam architecto optio quidem aliquid dicta! Iusto
+				cupiditate sunt voluptatibus sint, quidem consequuntur quisquam quo eius
+				aut, libero aspernatur corporis! Dignissimos consectetur, architecto a
+				repellendus inventore magni rerum? Laborum ea, dolores, repellendus unde
+				facilis suscipit dicta molestiae iusto recusandae, deleniti est eligendi
+				magni maiores voluptatum quae cumque inventore? Blanditiis atque
+				dignissimos tempore, aperiam molestiae illo numquam asperiores et
+				tenetur. Voluptatem dolor dolorum, neque aliquam sequi nam eum
+				reiciendis animi sapiente rerum est impedit reprehenderit sunt amet
+				eveniet optio voluptatum ab ipsa? Aperiam pariatur rerum tenetur
+				cupiditate adipisci ipsum?
+			</p>
+		</main>
+	);
+}
